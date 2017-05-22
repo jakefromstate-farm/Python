831bc4bbb9725335d5ad0e5ab38579d26b63620b
jakefromstate-farm <ctaps@compscis-MacBook-Pro-9.local>
2017-05-22 08:31:54 -0700

    wow

---
+++ wow.py
@@ -0,0 +1,10 @@
+print("My name is Ramon Zeidi.")
+print("My student ID is 2049061.")
+print("R")
+print("a")
+print("m")
+print("o")
+print("n")
+print("My name all capitalized is RAMON ZEIDI.")
+my_summer_plans = ["This summer I plan to sleep, travel, work."]
+print (*my_summer_plans)
